183fdb8bc6f6182879396346f8ed4fc5190753f5
Gabriel Willms <willms75@gmail.com>
2024-01-08 17:26:11 -0300

    Add comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/gwillms.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gabriel Willms</strong>
+                            <time title='04 de janeiro às 10:20' dateTime='2024-01-04 10:20:00'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! </p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
